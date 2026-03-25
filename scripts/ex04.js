b9604e234687f797b9aef06da34246cc7281825c
Bruno <bruno_d_luiz@estudante.sesisenai.org.br>
2026-03-25 12:33:07 -0300

    add - atividade finalizada

--- scripts/ex04.js
+++ scripts/ex04.js
@@ -1,13 +1,9 @@
-function ordenar(){
-
-let Numeros = [];
-
-for(let i = 1; i <= 4; i++){
-
-    Numeros.push = (Number(prompt("informe o valor: ")));
-
-    }
-Numeros.sort((a, b)) { Numeros.sort((a, b)) => a - b};
-alert("os numeros em ordem decrscemte são: +" + Numeros.join (", "));
-
+function Vetor(){
+     let numeros = [];
+     for (let i = 1; i < 5; i++){
+     numeros.push(Number(prompt("Informe o valor:" + i)));     
+     }
+     numeros.sort((a, b) => b - a);
+     alert("Os números em ordem decrescente são: " + numeros.join(", "));
+   
 }
